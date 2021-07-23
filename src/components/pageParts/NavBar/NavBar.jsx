e52f436983183d43b2d4cf46a91cf0efebdb1262
Jan Carlo Severino Santana <jseverino@arssenasa.gov.do>
2021-07-23 16:33:52 -0400

    first commit

---
+++ src/components/pageParts/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import './NavBar.css'
+const NavBar = () => {
+
+    return (
+        <div className="bar">
+            <nav>
+                <a href="">Inicio</a>
+                <a href="">About me</a>
+                <a href="">How i code</a>
+                <a href="">Contact</a>
+            </nav>
+        </div>
+
+    )
+}
+export default NavBar;
